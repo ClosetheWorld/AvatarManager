250d0c25e5c17f009b26f115c07cf72616bdfbbe
じゅううん <develop@closetheworld.net>
2025-02-23 00:59:49 +0900

    Revert "Revert "✨ OwnedAvatarテーブルへのカラムの追加と起動時にDBマイグレーションを行うコードの追加 (#19)" (#22)"
    This reverts commit 2ea12f5b3444ee72efcc08e75dc8bb05413ee194.

---
+++ AvatarManager.Core/Migrations/20250220080134_add-displayname-on-ownedavatar.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AvatarManager.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class adddisplaynameonownedavatar : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DisplayName",
+                table: "OwnedAvatars",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DisplayName",
+                table: "OwnedAvatars",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
