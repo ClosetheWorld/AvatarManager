60d7ca377b16d6188fff5fe189c96637148f13f0
ClosetheWorld <develop@closetheworld.net>
2025-03-04 18:11:03 +0900

    :recycle: 1ファイル1クラスのルールに準拠

---
+++ AvatarManager.Core/Infrastructures/Database/ApplicationDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace AvatarManager.Core.Infrastructures.Database;
+
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var connection = new SqliteConnection("Data Source=AvatarManager.db");
+        connection.Open();
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseSqlite(connection)
+            .Options;
+        return new ApplicationDbContext(options);
+    }
+}
+
