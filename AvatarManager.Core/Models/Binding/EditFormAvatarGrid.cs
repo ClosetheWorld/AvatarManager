c0684abd155ebbcfd2d9abdb0cbf49d41f3cfdd3
ClosetheWorld <develop@closetheworld.net>
2025-03-04 11:56:21 +0900

    :recycle: Bindingクラスを再定義

--- AvatarManager.Core/Models/Binding/EditFormAvatarGrid.cs
+++ AvatarManager.Core/Models/Binding/EditFormAvatarGrid.cs
@@ -1,16 +1,8 @@
-﻿using System.Drawing;
-
-namespace AvatarManager.Core.Models.Binding
+﻿namespace AvatarManager.Core.Models.Binding
 {
-    public class EditFormAvatarGrid
+    public class EditFormAvatarGrid : MainWindowAvatarGrid
     {
         public bool IsSelected { get; set; }
-
-        public Bitmap AvatarThumbnail { get; set; }
-
-        public string AvatarName { get; set; }
-
-        public string AvatarId { get; set; }
     }
 }
 
---
+++ AvatarManager.Core/Models/Binding/MainWindowAvatarGrid.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace AvatarManager.Core.Models.Binding
+{
+    public class MainWindowAvatarGrid
+    {
+        public Bitmap AvatarThumbnail { get; set; }
+
+        public string AvatarName { get; set; }
+
+        public string AvatarId { get; set; }
+    }
+}
