9a387755ec174e0f7216abbd1a8151996372c668
ClosetheWorld <develop@closetheworld.net>
2025-03-11 14:19:13 +0900

    :sparkles: タイトルバーにバージョンを表示するように変更

--- AvatarManager.WinForm/Forms/MainWindow.cs
+++ AvatarManager.WinForm/Forms/MainWindow.cs
@@ -68,6 +68,7 @@ public partial class MainWindow : Form
         }
         InitializeComponent();
         userName.Text = _user.DisplayName;
+        Text = $"AvatarManager v{Application.ProductVersion}";
     }
 
     #region EventHandlers
