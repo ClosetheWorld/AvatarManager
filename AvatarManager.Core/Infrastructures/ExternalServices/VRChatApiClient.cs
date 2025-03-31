45f967f56c0239276231a3f08a206e1abebb325e
じゅううん <develop@closetheworld.net>
2025-03-31 15:11:55 +0900

    :rocket: v1.0.10リリース準備 (#75)

--- AvatarManager.Core/Infrastructures/ExternalServices/VRChatApiClient.cs
+++ AvatarManager.Core/Infrastructures/ExternalServices/VRChatApiClient.cs
@@ -19,7 +19,7 @@ public class VRChatApiClient : IVRChatApiClient
         _configuration = new Configuration
         {
             BasePath = "https://api.vrchat.cloud/api/1",
-            UserAgent = "AvatarManager/1.0.9",
+            UserAgent = "AvatarManager/1.0.10",
             DefaultHeaders =
             {
                 ["Cookie"] = $"apiKey=JlE5Jldo5Jibnk5O5hTx6XVqsJu4WJ26; auth={authToken}"
@@ -28,7 +28,7 @@ public class VRChatApiClient : IVRChatApiClient
 
         _authenticationApi = new AuthenticationApi(_apiClient, _apiClient, _configuration);
         _httpClient.DefaultRequestHeaders.Add("Cookie", $"auth={authToken}");
-        _httpClient.DefaultRequestHeaders.Add("User-Agent", "AvatarManager/1.0.9");
+        _httpClient.DefaultRequestHeaders.Add("User-Agent", "AvatarManager/1.0.10");
     }
 
     public bool Auth()
